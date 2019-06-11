a7aed816dd21a38f4c3bba72288cfc36a7587b26
Erin Stalzer <erinstalzer@gmail.com>
2019-06-11 00:58:35 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ binding.pry
     user = User.find_by(:username => params[:username])
     if user && user.autheticate(params[:password])
       session[:user_id] = user.id
-      redirect "/account"
+      redirect to "/account"
     else
       redirect "/failure"
     end
