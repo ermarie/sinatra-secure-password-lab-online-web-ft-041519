31c7adb552d0edf3ee35e6bc142d0b35d18d7b7d
Erin Stalzer <erinstalzer@gmail.com>
2019-06-11 00:40:58 +0000

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/failure'
+    else
+      user = User.new(username: params[:username], password: params[:password])
+      user.save
+      redirect to '/login'
+    end
   end
 
   get '/account' do
@@ -32,7 +37,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+    binding.pry
+    if user
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
